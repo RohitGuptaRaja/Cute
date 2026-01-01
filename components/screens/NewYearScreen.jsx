f42ab0ea48b7ddad481eedbb17d41489e2cedb3b
RohitGuptaRaja <rohitkumardajar@gmail.com>
2026-01-01 10:26:07 +0530

    Update NewYearScreen.jsx

--- components/screens/NewYearScreen.jsx
+++ components/screens/NewYearScreen.jsx
@@ -8,7 +8,7 @@ export default function NewYearScreen() {
   const [showTop, setShowTop] = useState(true)
 
   return (
-    <div className="flex flex-col items-center justify-center h-full text-center px-4">
+    <div className="flex flex-col items-center justify-center min-h-screen text-center px-4">
 
       <h2 className="text-4xl md:text-5xl font-dancing-script text-white mb-8">
         Happy New Year my cutie pie 🎉💖
@@ -18,7 +18,7 @@ export default function NewYearScreen() {
       <div className="relative w-64 h-64 mb-10">
         {/* Bottom image */}
         <Image
-          src="/images/image2.jpg"
+          src="/image2.jpg"
           fill
           className="rounded-xl object-cover"
           alt="second"
@@ -38,7 +38,7 @@ export default function NewYearScreen() {
               exit={{ y: -400, opacity: 0 }}
             >
               <Image
-                src="/images/image1.jpg"
+                src="/image1.jpg"
                 fill
                 className="rounded-xl object-cover"
                 alt="first"
