9174cbd1fcb25e674772c1b9fe12238eb3017091
RohitGuptaRaja <rohitkumardajar@gmail.com>
2026-01-02 03:09:47 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,17 @@
-import { Quicksand } from "next/font/google";
-import "./globals.css";
+import { Quicksand } from "next/font/google"
+import "./globals.css"
+import BackgroundMusic from "@/components/BackgroundMusic"
 
 const quicksand = Quicksand({
   subsets: ["latin"],
-  weight:["300", "400", "500", "600", "700"],
-});
+  weight: ["300", "400", "500", "600", "700"],
+})
 
 export const metadata = {
   title: "You are special!",
-  description: "A cute little website filled with compliments, surprises, and a heartfelt message made just for you.",
-};
+  description:
+    "A cute little website filled with compliments, surprises, and a heartfelt message made just for you.",
+}
 
 export default function RootLayout({ children }) {
   return (
@@ -17,8 +19,12 @@ export default function RootLayout({ children }) {
       <body
         className={`${quicksand.className} bg-black antialiased select-none`}
       >
+        {/* 🎵 Background Music */}
+        <BackgroundMusic />
+
+        {/* App Content */}
         {children}
       </body>
     </html>
-  );
+  )
 }
